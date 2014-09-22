9e2242e1892331ce535e081a21bb204383b00ce0
AScott <atscott01@gmail.com>
2014-09-22 13:45:14 -0500

    trying to get passport oauth

--- app.js
+++ app.js
@@ -6,13 +6,45 @@
 // and jade as template engine (http://jade-lang.com/).
 
 var express = require('express'),
-  path = require('path');
+    path = require('path'),
+    config = require('./oauth.js'),
+    passport = require('passport'),
+    FacebookStrategy = require('passport-facebook').Strategy,
+    TwitterStrategy = require('passport-twitter').Strategy;
+
+passport.use(new FacebookStrategy({
+	clientID: config.facebook.clientID,
+	clientSecret: config.facebook.clientSecret,
+	callbackURL: config.facebook.callbackURL
+},
+function(accessToken, refreshToken, profile, done) {
+	process.nextTick(function () {
+		return done(null, profile);
+	});
+}
+));
+
+passport.use(new TwitterStrategy({
+	consumerKey: config.twitter.consumerKey,
+	consumerSecret: config.twitter.consumerSecret,
+	callbackURL: config.twitter.callbackURL
+},
+function(accessToken, refreshToken, profile, done) {
+	process.nextTick(function () {
+		return done(null, profile);
+	});
+}
+));
+
 
 // setup middleware
 var app = express();
 app.use(app.router);
+app.use(express.session({ secret: 'keyboard cat' }));
 app.use(express.errorHandler());
 app.use(express.static(__dirname + '/public')); //setup static public directory
+app.use(passport.initialize());
+app.use(passport.session());
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views'); //optional since express defaults to CWD/views
 
@@ -22,6 +54,43 @@ app.use(express.static(path.join(__dirname, '/lib')));
 // render index page
 app.get('/', function(req, res){
 	res.render('index');
+});
+
+
+// Redirect the user to twitter for authentication.  When complete,
+// Twitter will redirect the user back to the application at
+//     /auth/twitter/callback
+app.get('/auth/twitter', passport.authenticate('twitter'));
+
+// twitter will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+app.get('/auth/twitter/callback', 
+		passport.authenticate('twitter', { successRedirect: '/',
+			failureRedirect: '/login' }));
+
+// Redirect the user to Facebook for authentication.  When complete,
+// Facebook will redirect the user back to the application at
+//     /auth/facebook/callback
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+app.get('/auth/facebook/callback', 
+		passport.authenticate('facebook', { successRedirect: '/',
+			failureRedirect: '/login' }));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
 });
 
 // There are many useful environment variables available in process.env.
@@ -42,4 +111,9 @@ var port = (process.env.VCAP_APP_PORT || 3000);
 // Start server
 app.listen(port, host);
 console.log('App started on port ' + port);
+// test authentication
+function ensureAuthenticated(req, res, next) {
+	if (req.isAuthenticated()) { return next(); }
+	res.redirect('/')
+}
 
