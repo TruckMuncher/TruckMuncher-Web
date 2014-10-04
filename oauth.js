88eafae262cf944aad20c5540bc4cfc91cf9fc1a
Macon Suckow <suckowm@msoe.edu>
2014-10-03 20:56:40 -0500

    Started working on the menu item

--- oauth.js
+++ oauth.js
@@ -4,16 +4,20 @@ var host = (process.env.VCAP_APP_HOST || 'localhost');
 var port = (process.env.VCAP_APP_PORT || 3000);
 var url = JSON.parse(process.env.VCAP_APPLICATION || '{"uris":["' + host + ':' + port + '"]}').uris[0]
 //var url = process.env.VCAP_APPLICATION ? 'DevTruckMuncher.mybluemix.net' : host + ':' + port;
+
+var protocol = host === 'localhost' ? 'http://' : 'https://'
+
+
 var ids = {
 	facebook: {
 		clientID: '691864144229001',
-		clientSecret: '7d8f94b356f6540a440d53abd34e4f80',
-		callbackURL: 'https://' + url + '/auth/facebook/callback'
+		clientSecret: '',
+		callbackURL: protocol + url + '/auth/facebook/callback'
 	},
 	twitter: {
-		consumerKey: 'KCWZ6nlCEykT9S7AjGqJwEsM8',
-		consumerSecret: 'userPqxIrLjeKGDrURvZMWclgIWKsP5WslVEadMU7ii1cJtSqF',
-		callbackURL: 'https://' + url + '/auth/twitter/callback'
+		consumerKey: 'L2teeXzIVHPFsEb2tc7nBQ9wT',
+		consumerSecret: '',
+		callbackURL: protocol + url + '/auth/twitter/callback'
 	}
 };
 
