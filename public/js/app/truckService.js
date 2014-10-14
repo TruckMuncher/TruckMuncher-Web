68923d5203ceb82d2d5b4394715902e11f62c30d
atscott <atscott01@gmail.com>
2014-10-14 18:48:38 -0500

    fixing no data on getTrucksForVendor

--- public/js/app/truckService.js
+++ public/js/app/truckService.js
@@ -5,6 +5,7 @@ angular.module('TruckMuncherApp')
                 return $http({
                     method: 'POST',
                     url: 'https://api.truckmuncher.com:8443/com.truckmuncher.api.trucks.TruckService/getTrucksForVendor',
+                    data: {},
                     crossDomain: true,
                     cache: true
                 }).then(function(response){
