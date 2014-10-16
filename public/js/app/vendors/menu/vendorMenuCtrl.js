f1a9362ad7a0c1ca518777c058b3b28380e56e8d
atscott <atscott01@gmail.com>
2014-10-16 14:41:55 -0500

    added tests for menu

--- public/js/app/vendors/menu/vendorMenuCtrl.js
+++ public/js/app/vendors/menu/vendorMenuCtrl.js
@@ -1,15 +1,7 @@
 angular.module('TruckMuncherApp').controller('vendorMenuCtrl', ['$scope', 'MenuService', 'TruckService', '$state',
     function ($scope, MenuService, TruckService, $state) {
         $scope.selectedTruck = null;
-
-        $scope.menu = {"truckId": 1234, "menuId": 1234, "category": [
-            {"id": 1234, "name": "Sandwiches", "notes": "Free drink with all sandwiches", "orderInMenu": 0, "menuItem": [
-                {"id": 1234, "name": "Roast Beef", "price": 5.75, "notes": "Roast beef, lettuce, and tomato", "tag": ["tag1", "tag2"], "orderInCategory": 0, "isAvailable": true, "unknownFieldsSerializedSize": 0, "serializedSize": 69}
-            ], "unknownFieldsSerializedSize": 0, "serializedSize": 120},
-            {"id": 1235, "name": "Salads", "notes": "Free drink with all salads", "orderInMenu": 1, "menuItem": [
-                {"id": 1234, "name": "Worst Salad NA", "price": 1000.75, "notes": "Iceburg lettuce. Eat up!", "tag": ["tag1", "tag2"], "orderInCategory": 0, "isAvailable": true, "unknownFieldsSerializedSize": 0, "serializedSize": 69}
-            ], "unknownFieldsSerializedSize": 0, "serializedSize": 120}
-        ], "unknownFieldsSerializedSize": 0, "serializedSize": 128};
+        $scope.menu = {};
 
         TruckService.getTrucksForVendor().then(function (response) {
             $scope.trucks = response;
@@ -21,9 +13,8 @@ angular.module('TruckMuncherApp').controller('vendorMenuCtrl', ['$scope', 'MenuS
         $scope.$watch('selectedTruck', function () {
             if ($scope.selectedTruck && $scope.menu.truckId !== $scope.selectedTruck) {
                 MenuService.getMenu($scope.selectedTruck).then(function (response) {
+                    $scope.menu = response;
                 });
-            } else {
-//                $scope.menu = {};
             }
         });
 
--- test/jasmine/app/vendors/menu/vendorMenuCtrlTests.js
+++ test/jasmine/app/vendors/menu/vendorMenuCtrlTests.js
@@ -2,17 +2,55 @@ describe('TruckMuncherApp', function () {
     beforeEach(module('TruckMuncherApp'));
 
     describe('vendorMenuCtrl', function () {
-        var $scope;
+        var $scope, $q;
 
-        beforeEach(inject(function ($rootScope, $controller) {
+        var mockMenuService = {
+            getMenu: function(){
+                var deferred = $q.defer();
+                deferred.resolve({});
+                return deferred.promise;
+            }
+        };
+
+        var mockTruckService = {
+            getTrucksForVendor: function(){
+                var deferred = $q.defer();
+                deferred.resolve({});
+                return deferred.promise;
+            }
+        };
+
+        beforeEach(inject(function ($rootScope, $controller, _$q_) {
             $scope = $rootScope.$new();
-            $controller('vendorMenuCtrl', {$scope: $scope});
+            $q = _$q_;
+            $controller('vendorMenuCtrl', {$scope: $scope, MenuService: mockMenuService, TruckService: mockTruckService});
         }));
 
         it('should update the menu when there is a "menuUpdated" notification', function () {
             $scope.menu = null;
             $scope.$emit('menuUpdated', {});
             expect($scope.menu).toEqual({});
+        });
+
+        it('should update the selected truck when there is a "menuUpdated" notification', function () {
+            $scope.menu = null;
+            $scope.$emit('menuUpdated', {truckId: 'abc'});
+            expect($scope.selectedTruck).toEqual('abc');
+        });
+
+        it('should request the menu for the truck when the selectedTruck changes', function(){
+            spyOn(mockMenuService, 'getMenu').andCallThrough();
+            $scope.selectedTruck = 'abcd';
+            $scope.$apply();
+            expect(mockMenuService.getMenu).toHaveBeenCalled();
+        });
+
+        it('should NOT request the menu for the truck when selectedTruck changes but the menu for that truck is being displayed already', function(){
+            $scope.menu.truckId = 'abcd';
+            spyOn(mockMenuService, 'getMenu');
+            $scope.selectedTruck = 'abcd';
+            $scope.$apply();
+            expect(mockMenuService.getMenu).not.toHaveBeenCalled();
         });
     });
 
