e29ba0f24d3fb2f1f78cbebbe138c57812cb6bde
atscott <atscott01@gmail.com>
2014-10-20 22:06:14 -0500

    got that menu working

--- public/js/app/menuService.js
+++ public/js/app/menuService.js
@@ -13,8 +13,8 @@ angular.module('TruckMuncherApp')
             },
             getItem: function (itemId) {
                 var url = 'https://api.truckmuncher.com:8443/com.truckmuncher.api.menuadmin.MenuAdminService/getMenuItem';
-                var data = {'itemId': itemId};
-                return httpHelperService.post(url, data, 'item');
+                var data = {'menuItemId': itemId};
+                return httpHelperService.post(url, data, 'menuItem');
             },
             getCategory: function (categoryId) {
                 var url = 'https://api.truckmuncher.com:8443/com.truckmuncher.api.menuadmin.MenuAdminService/getCategory';
@@ -29,6 +29,16 @@ angular.module('TruckMuncherApp')
             addOrUpdateItem: function(item, truckId, categoryId) {
                 var url = 'https://api.truckmuncher.com:8443/com.truckmuncher.api.menuadmin.MenuAdminService/modifyMenuItem';
                 var data = {'menuItem': item, 'truckId': truckId, 'categoryId': categoryId};
+                return httpHelperService.post(url, data, 'menu');
+            },
+            deleteCategory: function(truckId, categoryId){
+                var url = 'https://api.truckmuncher.com:8443/com.truckmuncher.api.menuadmin.MenuAdminService/deleteCategory';
+                var data = {'truckId': truckId, 'categoryId': categoryId};
+                return httpHelperService.post(url, data, 'menu');
+            },
+            deleteItem: function(truckId, menuItemId){
+                var url = 'https://api.truckmuncher.com:8443/com.truckmuncher.api.menuadmin.MenuAdminService/deleteMenuItem';
+                var data = {'truckId': truckId, 'menuItemId': menuItemId};
                 return httpHelperService.post(url, data, 'menu');
             }
         };
