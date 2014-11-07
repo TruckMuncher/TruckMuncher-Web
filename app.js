f315b36311b97e792d4bf0a9b71002c5625e7758
atscott <atscott01@gmail.com>
2014-11-07 11:15:39 -0600

    oh...the solution was there

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.use(express.errorHandler());
 app.use(function (req, res, next) {
     res.setHeader('X-Frame-Options', 'DENY');
     res.setHeader('X-Content-Type-Options', 'nosniff');
-    res.setHeader("Cache-Control", "no-cache,no-store");
+    res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate, private");
     res.setHeader("Pragma", "no-cache");
 
     res.locals.sessionToken = req.session.sessionToken;
