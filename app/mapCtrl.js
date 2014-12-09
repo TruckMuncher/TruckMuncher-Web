d5f40f8db7f6b5f4947a66d61e6040eab95c708c
Macon Suckow <suckowm@msoe.edu>
2014-12-08 22:01:52 -0600

    Basic map is working

--- app/mapCtrl.js
+++ app/mapCtrl.js
@@ -2,45 +2,51 @@
  * Created by maconsuckow on 12/3/14.
  */
 angular.module('TruckMuncherApp').controller('mapCtrl', ['$scope', 'TruckService', 'uiGmapGoogleMapApi',
-    function ($scope, TruckService, uiGmapGoogleMapApi) {
+    function ($scope, TruckService) {
 
+        //Latitude and Longitude varaibles
         var lat;
         var lon;
 
+        //Initialization of the map with a point to load first
         $scope.map = {
             center: {
-                latitude: 43.05, longitude: -87.95 },
+                latitude: 43.05,
+                longitude: -87.95
+            },
             zoom: 12
         };
+
+        //Create markers array
         $scope.randomMarkers = [];
 
+        //The navigator function that allows the gps coordinates to be gathered by the browser
         navigator.geolocation.getCurrentPosition(function(pos) {
 
+            //Sets current latitude and longitude to the variables
             lat = pos.coords.latitude;
             lon = pos.coords.longitude;
 
-            var myLatLng = new google.maps.LatLng(lat, lon);
-
+            //Resets the maps center
             $scope.map.center = { latitude: lat, longitude: lon};
 
             getMarkers();
 
+            //Apply the changes to the scope parameter
             $scope.$apply();
         }, function(error) {
             alert('Unable to get location: ' + error.message);
         });
 
+        //Function that gets back the currently active trucks and creates google map markers from them
         function getMarkers() {
             TruckService.getActiveTrucks(lat, lon).then(function (response) {
 
                 trucks = response;
-
                 var markers = [];
 
                 for (var i = 0; i < trucks.length; i++) {
-
                     var temp = populateMarker(trucks[i]);
-
                     markers.push(temp);
                 }
 
@@ -49,32 +55,26 @@ angular.module('TruckMuncherApp').controller('mapCtrl', ['$scope', 'TruckService
             });
         }
 
+        //Function that receives a truck and strips the necessary information and returns a new marker
+        //id: needed for the info windows
+        //latitude: Latitude of the marker
+        //longitude: Longitude of the marker
+        //show: display of the info window when marker is created
+        //options: Info window options. Contains content and a max-width
         function populateMarker(truck) {
             var newMarker = {
                 id: truck.id,
                 latitude: truck.latitude,
                 longitude: truck.longitude,
-                title: truck.id,
-                show: false
-            };
-
-            newMarker.onClick = function() {
-                console.log("Clicked");
-                newMarker.show = !newMarker.show;
+                show: false,
+                options: {
+                    content: "<b>A Food Truck</b>" +
+                        "<p>Chinese Cuisine</p>" +
+                        "<p style='min-width: 150px'>Hours: 11am - 6pm</p>",
+                    maxWidth: 150
+                }
             };
 
             return newMarker;
         }
-
-        $scope.windowOptions = {
-            visible: true
-        };
-
-        $scope.closeClick = function() {
-            console.log("closeClick");
-            $scope.windowOptions.visible = false;
-        };
-
-        $scope.title = "Window Title!";
-
     }]);
