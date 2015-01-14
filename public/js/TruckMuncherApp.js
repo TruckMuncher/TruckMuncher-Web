17b4f9fbe25c22bcd118a99e66500a77ad25318a
atscott <atscott01@gmail.com>
2015-01-14 13:27:17 -0600

    removing mock of getActiveTrucks and putting real api call in

--- public/js/TruckMuncherApp.js
+++ public/js/TruckMuncherApp.js
@@ -694,18 +694,9 @@ angular.module('TruckMuncherApp').controller('mapCtrl', ['$scope', 'TruckService
                 return httpHelperService.getApiUrl() + '/com.truckmuncher.api.file.FileService/uploadFile/' + truckId;
             },
             getActiveTrucks: function (latitude, longitude) {
-                //var url = httpHelperService.getApiUrl() + '/com.truckmuncher.api.trucks.TruckService/getActiveTrucks';
-                //var data = {'latitude': latitude, 'longitude': longitude};
-                //return httpHelperService.post(url, data, 'trucks');
-
-                var deferred = $q.defer();
-                deferred.resolve([
-                    {id: "2d1dada3-80f1-4c0e-b878-a02626aafea7", latitude: 43.05, longitude: -87.95},
-                    {id: "2d1dada3-80f1-4c0e-b878-a02626aafea6", latitude: 43.046978, longitude: -87.904087},
-                    {id: "2d1dada3-80f1-4c0e-b878-a02626aafea5", latitude: 43.044093, longitude: -87.902027},
-                    {id: "2d1dada3-80f1-4c0e-b878-a02626aafea4", latitude: 43.045849, longitude: -87.899795}
-                ]);
-                return deferred.promise;
+                var url = httpHelperService.getApiUrl() + '/com.truckmuncher.api.trucks.TruckService/getActiveTrucks';
+                var data = {'latitude': latitude, 'longitude': longitude};
+                return httpHelperService.post(url, data, 'trucks');
             },
             getTruckProfiles: function (latitude, longitude) {
                 var url = httpHelperService.getApiUrl() + '/com.truckmuncher.api.trucks.TruckService/getTruckProfiles';
