80d6fb02c56cbffb461e881444c552c7bdcb7451
atscott <atscott01@gmail.com>
2015-01-25 20:10:39 -0600

    variable scopes...

--- server/serverConfig.js
+++ server/serverConfig.js
@@ -5,11 +5,13 @@ var fs = require('fs'),
 var port = (process.env.VCAP_APP_PORT || 3000);
 var host = (process.env.VCAP_APP_HOST || 'localhost');
 
+var privateKey, certificate, chain, credentials;
+
 if (host !== 'localhost') {
-    var privateKey = fs.readFileSync('/etc/ssl/truckmuncher-web/truckmuncher_web.key', 'utf8');
-    var certificate = fs.readFileSync('/etc/ssl/truckmuncher-web/truckmuncher_com.crt', 'utf8');
-    var chain = fs.readFileSync('/etc/ssl/truckmuncher-web/truckmuncher_com_intermediates.pem', 'utf8');
-    var credentials = {key: privateKey, cert: certificate, ca: [chain]};
+    privateKey = fs.readFileSync('/etc/ssl/truckmuncher-web/truckmuncher_web.key', 'utf8');
+    certificate = fs.readFileSync('/etc/ssl/truckmuncher-web/truckmuncher_com.crt', 'utf8');
+    chain = fs.readFileSync('/etc/ssl/truckmuncher-web/truckmuncher_com_intermediates.pem', 'utf8');
+    credentials = {key: privateKey, cert: certificate, ca: [chain]};
 }
 
 var httpsServer, httpServer;
