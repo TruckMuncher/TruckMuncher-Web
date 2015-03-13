f3046fecda029252df47f956dd2859ef504fd771
atscott <atscott01@gmail.com>
2015-03-12 19:04:44 -0500

    only showing unique search results

--- app/truckProfilesCtrl.ts
+++ app/truckProfilesCtrl.ts
@@ -9,10 +9,10 @@ interface ITruckProfilesScope extends ng.IScope {
 
 angular.module('TruckMuncherApp').controller('truckProfilesCtrl', ['$scope', 'TruckProfileService', 'SearchService', '$analytics',
     function ($scope:ITruckProfilesScope, TruckProfileService:ITruckProfileService, SearchService:ISearchService, $analytics:IAngularticsService) {
-        var allTrucks: Array<ITruckProfile> = TruckProfileService.allTrucksFromCookie();
+        var allTrucks:Array<ITruckProfile> = TruckProfileService.allTrucksFromCookie();
         $scope.displayedTrucks = TruckProfileService.allTrucksFromCookie();
 
-        TruckProfileService.updateTruckProfiles().then(function (response) {
+        TruckProfileService.updateTruckProfiles().then((response) => {
             allTrucks = response;
             $scope.displayedTrucks = response;
         });
@@ -22,15 +22,18 @@ angular.module('TruckMuncherApp').controller('truckProfilesCtrl', ['$scope', 'Tr
         $scope.simpleSearch = (query) => {
             $scope.loading = true;
 
-            SearchService.simpleSearch(query, 100, 0).then((results)=> {
-                $scope.displayedTrucks = _.map(results.searchResponse, (truck) => {
-                    return truck.truck;
-                });
+            SearchService.simpleSearch(query, 100, 0).then((results) => {
+                $scope.displayedTrucks =
+                    _.chain(results.searchResponse)
+                        .map((item) => { return item.truck; })
+                        .uniq('id')
+                        .value();
                 $scope.loading = false;
             });
             $analytics.eventTrack('SimpleSearch', {category: 'TruckProfiles', label: query});
         };
-        $scope.$watch('searchQuery', function () {
+
+        $scope.$watch('searchQuery', () => {
             if ($scope.searchQuery !== undefined && $scope.searchQuery.length === 0 && $scope.displayedTrucks.length < allTrucks.length) {
                 $scope.displayedTrucks = allTrucks;
 
