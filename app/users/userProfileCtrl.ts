97dcbaa90ac1e649cfc1b270398d3e4df090bf1a
atscott <atscott01@gmail.com>
2015-04-04 16:03:01 -0500

    added ability to create truck from profile

--- app/users/userProfileCtrl.ts
+++ app/users/userProfileCtrl.ts
@@ -3,9 +3,11 @@ interface IUserProfileCtrlScope {
     submit(): void;
     user: IUser;
     requestInProgress: boolean;
+    createTruck():void;
+    unlinkAccounts(unlinkTwitter:boolean, unlinkFacebook:boolean): void;
 }
-angular.module('TruckMuncherApp').controller('userProfileCtrl', ['$scope', 'StateService', 'UserService',
-    function ($scope:IUserProfileCtrlScope, StateService, UserService) {
+angular.module('TruckMuncherApp').controller('userProfileCtrl', ['$scope', 'StateService', 'UserService', 'TruckService', 'growl',
+    function ($scope:IUserProfileCtrlScope, StateService, UserService, TruckService, growl: IGrowlService) {
         $scope.isVendor = StateService.isVendor();
         $scope.requestInProgress = false;
 
@@ -19,9 +21,29 @@ angular.module('TruckMuncherApp').controller('userProfileCtrl', ['$scope', 'Stat
                 UserService.modifyAccount($scope.user.postToFb, $scope.user.postToTw).then((response)=> {
                     $scope.user = response;
                     $scope.requestInProgress = false;
+                    growl.addSuccessMessage('Settings saved');
                 }, ()=> {
                     $scope.requestInProgress = false;
                 });
             }
+        };
+
+        $scope.createTruck = () => {
+            $scope.requestInProgress = true;
+            TruckService.modifyTruckProfile(null, 'New Truck', null, null, null, null, null).then(()=> {
+                $scope.isVendor = true;
+                $scope.requestInProgress = false;
+            }, ()=> {
+                $scope.requestInProgress = false;
+            });
+        };
+
+        $scope.unlinkAccounts = (unlinkTwitter, unlinkFacebook)=> {
+            $scope.requestInProgress = true;
+            UserService.unlinkAccounts(unlinkTwitter, unlinkFacebook).then(()=>{
+                $scope.requestInProgress = false;
+            },()=>{
+                $scope.requestInProgress = false;
+            });
         }
     }]);
